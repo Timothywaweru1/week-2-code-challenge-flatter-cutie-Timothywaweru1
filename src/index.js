512106eee19beed306874d4b97077f54e03e2377
Timothy Waweru <timothy.waweru1@student.moringaschool.com>
2025-03-23 23:57:28 +0300

    made name show

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-// Your code
-let baseUrl = "http://localhost:3000/characters";
+let baseUrl = "http://localhost:3000/characters/1"
 fetch (baseUrl)
-.then (res => res)
-.then (data => console.log (data))
-.catch (error => error)
+.then (response => response.json ())
+.then (data => displayName(data) )
+
+function showName () {
+    let show = document.querySelector ("#name")
+    show.innerHTML = `<p id="name">Mr.Cute</p> `
+
+}
+function displayName () {
+    let top = document.querySelector ("#text1")
+    top.addEventListener ("click",showName) 
+
+}
+function displayImage () {
+    let img = document.querySelector ("img")
+    img.src = "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif"
+    
+}
+displayImage()
+
+// function showNameOfMonkey () {
+//     const show = document.querySelector ("#name")
+//     show.innerHTML = `<p id="name">Mx.Monkey</p>`
+// }
+
+
+// function displayName () {
+//     let show = document.querySelector ("#text2")
+//     show.addEventListener ("click",showName)
+// }
+// function showMonkey (name) {
+// let show = document.querySelector ("#text2")
+// show.addEventListener ("click",showNameOfMonkey)
+
+// }
+
+    
+
+
